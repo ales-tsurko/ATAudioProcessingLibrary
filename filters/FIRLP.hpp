f9f5efef4f04dedb5a058c4961260bfceaf4e99a
Ales Tsurko <ales.tsurko@gmail.com>
2016-02-19 16:30:24 +0300

    Initial commit

---
+++ filters/FIRLP.hpp
@@ -0,0 +1,38 @@
+//
+//  FIRLP.hpp
+//  AUSaturatedAmp
+//
+//  Created by Ales Tsurko on 16.02.16.
+//  Copyright © 2016 Aliaksandr Tsurko. All rights reserved.
+//
+
+#ifndef FIRLP_hpp
+#define FIRLP_hpp
+
+#include <stdio.h>
+#import "Globals.hpp"
+
+using namespace amp;
+
+class FIRLP : public Generator {
+public:
+    sample_vec_t output;
+    
+    void init(sample_t sampleRate,
+              int chnum,
+              int blockSize,
+              sample_t cuttoff,
+              int kernelSize);
+    
+    sample_vec_t calculateBlock(sample_vec_t input);
+    
+private:
+    sample_t cuttoffRatio;
+    sample_vec_t filterKernel;
+    int filterKernelSize;
+    sample_vec_t ringBuf;
+    size_t ringBufSize;
+    int ringBufIndexOffset;
+};
+
+#endif /* FIRLP_hpp */
